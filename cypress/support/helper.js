a0a9e305d4e6cf67509d83b896e7e89d979fbf24
OlyaProdaniuk <digitaliz29@gmail.com>
2023-06-24 11:13:11 +0200

    initial commit

---
+++ cypress/support/helper.js
@@ -0,0 +1,63 @@
+export function findProduct(productName) {
+  cy.get(".mat-grid-list").then((body) => {
+    if (body.find(`.item-name:contains(${productName})`).length > 0) {
+      cy.get(`.item-name:contains(${productName})`).click();
+    } else {
+      cy.get('[aria-label="Next page"]').click();
+      findProduct(productName);
+    }
+  });
+}
+
+export function headlessSignup(user) {
+  cy.request({
+    method: "POST",
+    url: "/api/Users",
+    body: {
+      email: user.email,
+      password: user.password,
+      passwordRepeat: user.password,
+      securityQuestion: {
+        id: 7,
+        question: "Name of your favorite pet?",
+      },
+      securityAnswer: user.answer,
+    },
+  }).then((response) => {
+    expect(response.status).to.be.equal(200);
+
+    const userId = response.body.id;
+
+    cy.request({
+      method: "POST",
+      url: "/api/SecurityAnswers",
+      body: {
+        UserId: userId,
+        answer: user.answer,
+        SecurityQuestionId: 7,
+      },
+    }).then((response) => {
+      expect(response.status).to.be.equal(200);
+    });
+  });
+}
+
+export function headlessLogin(user) {
+  cy.log(`Auth user with email: ${user.email} and password: ${user.password}`);
+
+  cy.request({
+    method: "POST",
+    url: "/rest/user/login",
+    body: {
+      email: user.email,
+      password: user.password,
+    },
+  }).then((response) => {
+    expect(response.status).to.be.equal(200);
+    let token = response.body.authentication.token;
+    let bid = response.body.authentication.bid;
+    cy.setCookie("token", token);
+    window.localStorage.setItem("token", token);
+    window.sessionStorage.setItem("bid", bid);
+  });
+}
